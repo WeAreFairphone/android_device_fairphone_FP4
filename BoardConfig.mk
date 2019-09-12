82ac4351f86ae9d2956cfc2b090c9f3fc8434176
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 23:47:56 +0200

    FP4: Enable updating of APEXes
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 BOARD_VENDOR := fairphone
 DEVICE_PATH := device/fairphone/FP4
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Include GSI keys
 $(call inherit-product, build/make/target/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/fairphone/FP4/FP4-vendor.mk)
 
