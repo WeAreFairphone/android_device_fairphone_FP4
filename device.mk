d70ce2963bc43272a8ef961f1d36415bf1060feb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:08:35 +0100

    FP4: Build Telephony packages
    Change-Id: Ia5eb8c58d0badf90a33e958eee1ff200c9be20d5

--- device.mk
+++ device.mk
@@ -281,6 +281,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
