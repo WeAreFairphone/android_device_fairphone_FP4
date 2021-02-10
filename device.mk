63afb8d86197b822127f2ce10042300573df4f58
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:02:15 +0100

    FP4: Build libhwbinder
    * Some libs uses it
    
    Change-Id: Ic40f730a4d498725459785b8c33ef7f5e8d0b13b

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
