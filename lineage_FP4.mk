a63f700391a62c7413435dd3324f480aedf83a5e
Alessandro Astone <ales.astone@gmail.com>
2021-03-09 18:15:41 +0100

    FP4: Set Bootanimation resolution

--- lineage_FP4.mk
+++ lineage_FP4.mk
@@ -14,6 +14,10 @@ $(call inherit-product, device/fairphone/FP4/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_FP4
 PRODUCT_DEVICE := FP4
