18b35f64d1192772513d9bb9bb076d35b7c56fbc
merothh <merothh@gmail.com>
2022-01-03 05:05:17 +0530

    FP4: Initial Overlay
    * Stock overlays from Fairphone/FP4eea/FP4:11/RKQ1.210503.001/FP3N:user/release-keys

--- device.mk
+++ device.mk
@@ -26,9 +26,11 @@ $(call inherit-product, vendor/fairphone/FP4/FP4-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Permissions
 PRODUCT_COPY_FILES += \
