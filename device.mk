2e0e568c7d80e7a26f151a3b28b9d1d938d6693e
merothh <merothh@gmail.com>
2022-01-02 20:25:57 +0530

    FP4: Build Fingerprint service

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
