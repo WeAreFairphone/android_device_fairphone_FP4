2c78f5ecb126c6760e828563cbcc598f3370ebb7
merothh <merothh@gmail.com>
2022-02-07 12:58:04 +0530

    FP4: Bump to FP3R.A.099.20220112

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,7 +194,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-11-05
+VENDOR_SECURITY_PATCH := 2021-12-05
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
