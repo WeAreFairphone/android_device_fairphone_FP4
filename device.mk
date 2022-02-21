c36e2ba17add885fa961be38225f935cb8209df6
Michael Bestas <mkbestas@gmail.com>
2022-02-22 00:04:50 +0200

    FP4: Convert init.recovery.qcom.rc to build target
    * Don't rely on lineage specific recovery file copying
    
    Change-Id: Id3773fabd06904823f4ea54ed18b9a17f2fc5f34

--- device.mk
+++ device.mk
@@ -371,6 +371,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.qti.qcv.rc \
+    init.recovery.qcom.rc \
     init.stnfc.rc \
     init.target.rc \
     fstab.default \
