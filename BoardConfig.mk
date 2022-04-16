3d30cd4b85e268445dfddff82a839bd528bdbfc6
Michael Bestas <mkbestas@lineageos.org>
2022-04-16 12:34:19 +0300

    FP4: Update to FP3T release
    Change-Id: I81ac14db924bccc17278e90af18226b03294c4b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,7 +193,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-12-05
+VENDOR_SECURITY_PATCH := 2022-03-05
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
